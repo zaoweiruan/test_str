512b68c474a7fd8e79ce06aee8fd7c9077a6961c
'flybearonsky' <'zaoweiruan@sohu.com'>
2024-10-23 12:30:28 +0800

    test1

--- sqlite3_test.cpp
+++ sqlite3_test.cpp
@@ -10,6 +10,28 @@
 #include <iomanip>
 using namespace std;
 static int titleWith=16;
+int ireturn_multi_serch(sqlite3 *db,const char *sql, int r, int c)
+{
+    char *errMsg;
+    char **dbResult;
+    int nRow = 0, nColumn = 0;
+    int rc;
+
+    int result;
+    rc = sqlite3_get_table(db,sql,&dbResult,&nRow,&nColumn,&errMsg);
+    if(rc == SQLITE_OK && r<=nRow &&r>0 && c<=nColumn &&c>0)
+    {
+        result = atoi(dbResult[r*nColumn+c-1]);
+        sqlite3_free_table(dbResult);
+        return result;
+    }
+    else
+    {
+        cout<<"第"<<r<<"行,第"<<c<<"条数据无效"<<endl;
+        return NULL;
+    }
+}
+
 static int callback(void *data, int argc, char **argv, char **azColName){
    int i;
    if (*((bool*)data)) {
@@ -39,12 +61,14 @@ int main(int argc, char* argv[])
 	   const char* data = "Callback function called";
 	   bool titleflag=true;
 
-   rc = sqlite3_open("E:/mvnworkspace/python/二期诊断接口轮询/broadband_users_t.db", &db);
-
+//   rc = sqlite3_open("E:/mvnworkspace/python/二期诊断接口轮询/broadband_users_t.db", &db);
+   cout<<sqlite3_open("E:/mvnworkspace/python/二期诊断接口轮询/broadband_users_t.db", &db)<<endl;
 
    /* Execute SQL statement */
    string sql = "SELECT rowid,* from fttr_all_user limit 10";
-   rc = sqlite3_exec(db, sql.c_str(), callback, (void*)&titleflag, &zErrMsg);
+   rc = ireturn_multi_serch(db, sql.c_str(),2,3);
+
+/*   rc = sqlite3_exec(db, sql.c_str(), callback, (void*)&titleflag, &zErrMsg);
    if (!zErrMsg)
    {
 	   cout<<zErrMsg<<endl;
@@ -56,7 +80,7 @@ int main(int argc, char* argv[])
       sqlite3_free(zErrMsg);
    }else{
 	   cout<<"Operation done successfully\n"<<endl;
-   }
+   }*/
    sqlite3_close(db);
    return 0;
 
